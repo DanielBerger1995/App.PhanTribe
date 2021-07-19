198b0342c5a35b68002393658405f6ddf9bf88e9
Daniel Berger <bergerdaniel1995@gmail.com>
2021-07-19 09:16:03 +0200

    commit2

--- js/mains.js
+++ js/mains.js
@@ -1,102 +1,33 @@
 "use strict";
 
-const _eventRef = _db.collection("Events");
-let _selectedImgFile = "";
-let _events = [];
-let _selectedEventId = "";
-let _firebaseUI;
-
-// ========== FIREBASE AUTH ========== //
-// Listen on authentication state change
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) { // if user exists and is authenticated
-        userAuthenticated(user);
-    } else { // if user is not logged in
-        userNotAuthenticated();
-    }
-});
-
-function userAuthenticated(user) {
-    appendUserData(user);
-    hideTabbar(false);
-    showLoader(false);
-}
-
-function userNotAuthenticated() {
-    hideTabbar(true);
-    showPage("login");
-
-    // Firebase UI configuration
-    const uiConfig = {
-        credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID
-        ],
-        signInSuccessUrl: '#home'
-    };
-    // Init Firebase UI Authentication
-    if (!_firebaseUI) {
-        _firebaseUI = new firebaseui.auth.AuthUI(firebase.auth());
-    }
-    _firebaseUI.start('#firebaseui-auth-container', uiConfig);
-    showLoader(false);
-}
-
-// show and hide tabbar
-function hideTabbar(hide) {
-    let tabbar = document.querySelector('#tabbar');
-    if (hide) {
-        tabbar.classList.add("hide");
-    } else {
-        tabbar.classList.remove("hide");
-    }
-}
+let _trials = [];
 
 
-// sign out user
-function logout() {
-    firebase.auth().signOut();
-}
+const _trialRef = _db.collection("user");
 
-function appendUserData(user) {
-    console.log(user);
-    document.querySelector('#user-data').innerHTML = `
-    <img class="profile-img" src="${user.photoURL || "img/placeholder.jpg"}">
-    <h3>${user.displayName}</h3>
-    <p>${user.email}</p>
-  `;
-}
-
-
-
-
-
-_eventRef.onSnapshot(function (snapshotData) {
-    _events = [];
+_trialRef.onSnapshot(function (snapshotData) {
+    _trials = [];
     snapshotData.forEach(function (doc) {
-        let event = doc.data();
-        event.id = doc.id;
-        _events.push(event);
+        let trial = doc.data();
+        trial.id = doc.id;
+        _trials.push(trial);
     });
-    appendEvents(_events);
+    appendTrials(_trials);
 });
-
-
 
 
 
 // append events to the DOM
-function appendEvents(events) {
+function appendTrials(trials) {
     let htmlTemplate = "";
-    for (let event of events) {
-        console.log(event);
+    for (let trial of trials) {
+
+
+        console.log(trial);
         htmlTemplate += `
         <article>
         
-          
-                <h2>${event.name}</h2>
+                <h2>${trial.NewTrial}</h2>
                 
         </article>
         `;
@@ -105,17 +36,6 @@ function appendEvents(events) {
     document.querySelector('#event-container').innerHTML = htmlTemplate;
 
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -129,9 +49,32 @@ function create() {
         name: nameInput.value,
 
     };
+    _userRef.doc(_currentUser.uid).set({
+        NewTrial: firebase.firestore.FieldValue.arrayUnion(newEvent)
+    }, {
+        merge: true
+    });
 
-    _eventRef.add(newEvent);
     navigateTo("home");
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
